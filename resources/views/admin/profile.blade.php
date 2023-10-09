468f3363e16bc2caebae9635b50772d47bb7d451
mustafa-00 <karimi.mu99@gmail.com>
2023-10-09 13:48:39 +0430

    empolye section added

--- resources/views/admin/profile.blade.php
+++ resources/views/admin/profile.blade.php
@@ -18,14 +18,14 @@
 
     <section class="section profile">
       <div class="row">
-        {{-- <div class="col-xl-4">
+        <div class="col-xl-4">
 
           <div class="card">
             <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
 
               <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-              <h2>Kevin Anderson</h2>
-              <h3>Web Designer</h3>
+              <h2>{{ $profile[0]->name }}</h2>
+              <h3>{{$profile[0]->role}}</h3>
               <div class="social-links mt-2">
                 <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
                 <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
@@ -35,7 +35,7 @@
             </div>
           </div>
 
-        </div> --}}
+        </div>
 
         <div class="col-xl-8">
 
