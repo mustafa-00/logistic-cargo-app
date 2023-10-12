d8dc4bb4683fea9ade05f6e10541e3734d038332
mustafa-00 <karimi.mu99@gmail.com>
2023-10-12 20:44:04 +0430

    product page table created

--- resources/views/admin/product.blade.php
+++ resources/views/admin/product.blade.php
@@ -12,5 +12,75 @@
         </nav>
     </div>
 
+    <section class="section profile">
+        <div class="row">
+            {{-- overview --}}
+
+            <div class="col-xl-12">
+                <div class="card">
+                    <div class="card-body pt-3">
+                        <ul class="nav nav-tabs nav-tabs-bordered">
+                            {{-- overview --}}
+                            <li class="nav-item">
+                                <button class="nav-link active" data-bs-toggle="tab"
+                                    data-bs-target="#profile-overview">Overview</button>
+                            </li>
+                        </ul>
+                        <div class="tab-content pt-2">
+
+                            <div class="tab-pane fade show active profile-overview" id="profile-overview">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title"></h5>
+                                        <!-- Table with hoverable rows -->
+                                        <table class="table table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th scope="col">Id</th>
+                                                    <th scope="col">Name</th>
+                                                    <th scope="col">Email</th>
+                                                    <th scope="col">Role</th>
+                                                    <th scope="col">Photo</th>
+                                                    <th scope="col">Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                    <tr>
+                                                        <th scope="row">1</th>
+                                                        <td>mustafa</td>
+                                                        <td>mustafakarimi@gmail.com</td>
+                                                        <td>admin</td>
+                                                        <td>
+                                                            <ul
+                                                                class="list-unstyled users-list m-0 avatar-group d-flex align-items-center">
+                                                                <li data-bs-toggle="tooltip" data-popup="tooltip-custom"
+                                                                    data-bs-placement="top"
+                                                                    class="avatar avatar-xs pull-up"
+                                                                    title="Lilian Fuller">
+                                                                    <img src="../assets_employe/img/avatars/5.png"
+                                                                    style="width: 40px" alt="Avatar"
+                                                                    class="rounded-circle" />
+                                                                </li>
+                                                            </ul>
+                                                        </td>
+                                                        <td>
+                                                        <a href="" title="Edite"><i class="bx bx-edit-alt me-1" style="font-size: 20px"></i></a>
+                                                        <a href="" title="Delete"><i class="bx bx-trash-alt me-1" style="font-size: 20px"></i></a>
+                                                        <a href="" title="View"><i class="bx bx-show-alt me-1" style="font-size: 20px"></i></a>
+                                                        </td>
+                                                    </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
 </main>
 @endsection
